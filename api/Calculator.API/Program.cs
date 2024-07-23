7d85fe2c0bcdd66e362a9472ab1797185e013b4a
mihai10001 <rvivits@gmail.com>
2024-07-23 16:37:25 +0300

    enable CORS, add problem details (simple exception middleware)

--- api/Calculator.API/Program.cs
+++ api/Calculator.API/Program.cs
@@ -2,7 +2,17 @@ using Calculator.API.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+
+builder.Services.AddProblemDetails();
+
 var app = builder.Build();
+
+app.UseCors("AllowAllOrigins");
 
 app.UseHttpsRedirection();
 
